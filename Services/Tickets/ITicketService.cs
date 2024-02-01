533ffb49c52d25ceda58da7086c29b34f52c18d8
Vasilis Koupourtiadis <89838633+VasilisKoupourtiadis@users.noreply.github.com>
2024-02-01 13:20:09 +0100

    Add TicketService

---
+++ Services/Tickets/ITicketService.cs
@@ -0,0 +1,11 @@
+﻿using tickets.Models;
+
+namespace tickets.Services.Tickets
+{
+    public interface ITicketService
+    {
+        void AddTicket(Ticket ticket);
+        Task<Ticket> GetTicketAsync(Guid Id);
+        Task<ICollection<Ticket>> GetTicketsAsync();
+    }
+}
---
+++ Services/Tickets/TicketService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using tickets.Data;
+using tickets.Models;
+
+namespace tickets.Services.Tickets;
+
+public class TicketService : ITicketService
+{
+    private readonly ApplicationContext context;
+
+    public TicketService(ApplicationContext context)
+    {
+        this.context = context;
+    }
+
+    public void AddTicket(Ticket ticket)
+        => context.Tickets.Add(ticket);
+
+    public async Task<Ticket> GetTicketAsync(Guid Id) =>
+        await context.Tickets
+        .Include(x => x.Employee)
+        .FirstOrDefaultAsync(x => x.Id.Equals(Id));
+
+    public async Task<ICollection<Ticket>> GetTicketsAsync() =>
+        await context.Tickets
+        .Include(x => x.Employee)
+        .ToListAsync();
+}
