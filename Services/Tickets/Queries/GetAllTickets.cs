542fc582635fa6a71f2a5a6bd97d8727bd44ee93
Vasilis Koupourtiadis <89838633+VasilisKoupourtiadis@users.noreply.github.com>
2024-02-05 12:48:12 +0100

    Add mapper and handler for getting all tickets

---
+++ Services/Tickets/Queries/GetAllTickets.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using MediatR;
+
+namespace tickets.Services.Tickets.Queries;
+
+public class GetAllTickets
+{
+    public class GetAllTicketsQuery : IRequest<ICollection<TicketsResult>> { }
+
+    public class TicketsResult
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string Status { get; set; } = string.Empty;
+
+        public EmployeeResult Employee { get; set; } = new EmployeeResult();
+    }
+
+    public class EmployeeResult
+    {
+        public Guid Id { get; set; }
+
+        public string FullName { get; private set; } = string.Empty;
+
+        public string Title { get; private set; } = string.Empty;
+    }
+
+    public class Handler : IRequestHandler<GetAllTicketsQuery, ICollection<TicketsResult>> 
+    {
+        private readonly IServiceManager serviceManager;
+
+        private readonly IMapper mapper;
+
+        public Handler(IServiceManager serviceManager, IMapper mapper)
+        {
+            this.serviceManager = serviceManager;
+            this.mapper = mapper;
+        }
+
+        public async Task<ICollection<TicketsResult>> Handle(GetAllTicketsQuery request, CancellationToken cancellationToken)
+        {
+            var tickets = await serviceManager.TicketService.GetTicketsAsync();
+
+            var result = mapper.Map<ICollection<TicketsResult>>(tickets);
+
+            return result;
+        }
+    }
+}
---
+++ Services/Tickets/TicketsMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using tickets.Models;
+using tickets.Services.Tickets.Queries;
+
+namespace tickets.Services.Tickets;
+
+public class TicketsMapper : Profile
+{
+    public TicketsMapper()
+    {
+        CreateMap<Ticket, GetAllTickets.TicketsResult>();
+
+        CreateMap<Employee, GetAllTickets.EmployeeResult>();
+    }
+}
