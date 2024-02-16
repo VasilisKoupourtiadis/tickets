f7742ec476bbbd8364df8b1fb5844ad5c85c9b89
Vasilis Koupourtiadis <89838633+VasilisKoupourtiadis@users.noreply.github.com>
2024-02-16 13:51:14 +0100

    Add UpdateTicketStatus command to ticket service

---
+++ Services/Tickets/Commands/UpdateTicketStatus.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using MediatR;
+
+namespace tickets.Services.Tickets.Commands;
+
+public class UpdateTicketStatus
+{
+    public class UpdateTicketStatusCommand : IRequest<Unit> 
+    { 
+        public Guid Id { get; set; }
+
+        public string Status { get; set; } = string.Empty;
+    }
+
+    public class Handler : IRequestHandler<UpdateTicketStatusCommand, Unit>
+    {
+        private readonly IServiceManager serviceManager;
+
+        public Handler(IServiceManager serviceManager)
+        {
+            this.serviceManager = serviceManager;
+        }
+
+        public async Task<Unit> Handle(UpdateTicketStatusCommand request, CancellationToken cancellationToken)
+        {
+            var ticket = await serviceManager.TicketService.GetTicketAsync(request.Id);
+
+            if(request.Status.ToLower() == "active")
+            {
+                ticket.SetClosedStatus();
+            }
+
+            ticket.SetActiveStatus();
+
+            await serviceManager.SaveAsync();
+
+            return Unit.Value;
+        }
+    }
+}
